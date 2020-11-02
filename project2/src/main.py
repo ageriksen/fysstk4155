e130aa872d3820ede17cb348cd74b2a3054321fb
ageriksen <annikage@protonmail.com>
2020-11-02 17:41:01 +0100

    that's all I can do for today. let's try to keep the ..thoughts at a minimum

--- project2/src/main.py
+++ project2/src/main.py
@@ -7,15 +7,18 @@ import systems.simplepoly as smpl
 import systems.franke as frnk
 
 import numpy as np
+import sklearn.linear_model as skl
 
 
 def main():
     np.random.seed(2020)
-    rows = 100; cols = 100; sigma = 1
-    maxdegree = 15; testRatio=.2
+    rows = 100; cols = 50; sigma = 1
+    maxdegree = 20; testRatio=.2
 
-    franke = frnk.FrankeRegression(gd.SGD, epochs=500, minibatches=100)
-    franke.Set(rows, cols, sigma) 
+    franke = frnk.FrankeRegression()
+    franke.SetRegressor(gd.SGD, epochs=100, minibatches=100)
+    franke.SetSklRegressor(skl.LinearRegression)
+    franke.SetSystem(rows, cols, sigma) 
     franke.Run( testRatio, maxdegree )
 
 
--- project2/src/systems/franke.py
+++ project2/src/systems/franke.py
@@ -67,11 +67,13 @@ class _FrankeBaseRegressor:
 
         return term1 + term2 + term3 + term4
 
-    def Set(self, rows, cols, sigma): 
+    def SetSystem(self, rows, cols, sigma): 
         row = np.linspace(0,1,rows)
         col = np.linspace(0,1,cols)
         self.row_mat, self.col_mat = np.meshgrid(col, row)
-        self.height_mat = self.FrankeFunction(self.row_mat, self.col_mat)
+        self.height_mat = \
+                self.FrankeFunction(self.row_mat, self.col_mat)\
+                + sigma*np.random.randn(rows, cols)
         self.row = self.row_mat.ravel()
         self.col = self.col_mat.ravel()
         self.height = self.height_mat.ravel()
@@ -86,55 +88,82 @@ class FrankeRegression(_FrankeBaseRegressor):
     values, x,y,z coordinates and such. 
     """
 
-    def __init__(self, regressor, **kwargs):
+    def __init__(self):
+        self.regressor = None
+        self.sklregressor = None
+
+    def SetRegressor(self, regressor, **kwargs):
         self.regressor = regressor(**kwargs)
+
+    def SetSklRegressor(self, sklregressor, **kwargs):
+        self.sklregressor = sklregressor(**kwargs)
 
     def Run(self,  testRatio=.2, maxdegree=10):
         assert self.setup, "You need to setup the system!"
+        assert self.regressor is not None, "Regressor not set"
+        assert self.sklregressor is not None, "skl regressor not set"
 
-        regtrain = []
-        regtest = []
-        regR2 = []
-        regTrainMSE = []
-        regTestMSE = []
+        regtrain        =   []
+        regtest         =   []
+        regR2           =   []
+        regTrainMSE     =   []
+        regTestMSE      =   []
 
-        skltrain = []
-        skltest = []
-        sklR2 = []
-        sklTrainMSE = []
-        sklTestMSE = []
+        skltrain        =   []
+        skltest         =   []
+        sklR2           =   []
+        sklTrainMSE     =   []
+        sklTestMSE      =   []
         
         for deg in tqdm(range(1, maxdegree)):
 
             features = self.PolyFeatures(self.row, self.col, deg)
-            train, test = self.TrainTestSplit(self.height,testRatio)
-            heightTrain=self.height[train]
-            heightTest=self.height[test]
-            featuresTrain=features[train]
-            featuresTest=features[test]
 
-            sklmodel = skl.LinearRegression()
-            sklmodel.fit(featuresTrain, heightTrain)
-            sklmodel.predict(featuresTrain)
-            skltrain.append(sklmodel.predict(featuresTrain))
-            skltest.append(sklmodel.predict(featuresTest))
-            sklR2.append(sklmodel.score(featuresTest, heightTest))
+            train, test = self.TrainTestSplit(self.height,testRatio)
+
+            heightTrain     =   self.height[train]
+            heightTest      =   self.height[test]
+
+            featuresTrain   =   features[train]
+            featuresTest    =   features[test]
+
+            #
+            self.sklregressor.fit(featuresTrain, heightTrain)
+
+            skltrain.append(self.sklregressor.predict(featuresTrain))
+            skltest.append(self.sklregressor.predict(featuresTest))
+            
+            sklR2.append(self.sklregressor.score(featuresTest, heightTest))
             sklTrainMSE.append(mean_squared_error(heightTrain, skltrain[-1]))
             sklTestMSE.append(mean_squared_error(heightTest, skltest[-1]))
 
+            #
             self.regressor.fit(featuresTrain, heightTrain)
+
             regtrain.append(featuresTrain@self.regressor.theta)
             regtest.append(featuresTest@self.regressor.theta)
+
             regR2.append(self.R2(heightTest, regtest[-1]))
             regTrainMSE.append(self.singleMSE(heightTrain, regtrain[-1]))
             regTestMSE.append(self.singleMSE(heightTest, regtest[-1]))
 
+        regtrain        =   np.array(   regtrain        )
+        regtest         =   np.array(   regtest         )
+        regR2           =   np.array(   regR2           )
+        regTrainMSE     =   np.array(   regTrainMSE     )
+        regTestMSE      =   np.array(   regTestMSE      )
+        skltrain        =   np.array(   skltrain        )
+        skltest         =   np.array(   skltest         )
+        sklR2           =   np.array(   sklR2           )
+        sklTrainMSE     =   np.array(   sklTrainMSE     )
+        sklTestMSE      =   np.array(   sklTestMSE      )
+
+
         degrees = np.arange(1, maxdegree)
-        print("degrees:\n", degrees)
         plt.plot(degrees, sklTrainMSE, label='skl train')
         plt.plot(degrees, sklTestMSE, label='skl test')
-        #plt.plot(degrees, regTrainMSE, label='SGD train')
-        #plt.plot(degrees, regTestMSE, label='SGD test')
+        plt.plot(degrees, regTrainMSE, label='SGD train')
+        plt.plot(degrees, regTestMSE, label='SGD test')
         plt.legend()
         plt.xlabel('model complexity')
         plt.ylabel('MSE')
