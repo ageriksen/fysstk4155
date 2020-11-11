5b3a7d94abc58ff511efa9c0e115b120f2c4f8ac
ageriksen <annikage@protonmail.com>
2020-11-11 22:56:23 +0100

    forgot something

---
+++ prj2aworking/lib/gradientdescent.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import numpy as np
+from tqdm import tqdm
+
+class _gradientDescent:
+    def __init__(self):
+        self.null = False
+
+    def gradient(self, X, y, beta):
+        """
+        gradient for OLS, 
+        del(C) = del( (1/n)||X@beta - y||_2^2 = (2/n) X^T @ ( X@beta - y )
+        """
+        gradient = (2./y.shape[0])* X.T @ (X@beta - y)
+        if np.linalg.norm(gradient) <= 1e-5:
+            self.null = True
+        return gradient
+
+class SGD(_gradientDescent):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.learning = None
+        self.t = None
+        self.lrnmode = {\
+                'static': self.static, 
+                'dynamic': self.dynamic}
+
+    def SetLearningMode(self, mode, t):
+        """
+        sets the learning rate to either 'static' or 'dynamic' and the 
+        rate to either the static fraction t, or the list of the t0 and t1 
+        for the dynamic case
+        """
+        assert mode in self.lrnmode, mode + " input, needs 'static' or 'dynamic'"
+        self.learning = self.lrnmode[mode]
+        self.t = t
+
+    def dynamic(self, t): return self.t[0] / (t + self.t[1])
+    def static(self, t): return self.t
+
+    def fit(self, X, y, minibatches, epochs, lrn):
+        assert self.learning is not None, "need to set learning mode"
+
+        self.null = False
+        batchsize = int(y.shape[0]/minibatches)
+        theta = np.random.randn(X.shape[1])
+        self.t = lrn
+        for epoch in range(epochs):
+            for batch in range(minibatches):
+                rand = np.random.randint(minibatches)
+                batch = rand*batchsize
+                Xi = X[batch:batch+batchsize]; yi = y[batch:batch+batchsize]
+                gradients = self.gradient(Xi, yi, theta) 
+                eta = self.learning(epoch*minibatches+batch) 
+                theta -= eta*gradients
+            if self.null:
+                print("found a 0")
+                break
+        self.theta = theta
+        
+class GD(_gradientDescent):
+
+    def __init__(self, learningrate, *args):
+        self.learningrate = learningrate
+        super().__init__(*args)
+
+    def betaNew(self, X, y, betaOld):
+        return betaOld - self.learningrate*self.gradient(X, y, betaOld)
+
+    def FindBeta(self,X,y, maxiter=1e3):
+        beta = np.zeros(X.shape[1])#.reshape((X.shape[1],1))
+        self.null = False
+        for i in tqdm(range(int(maxiter))):
+            betaOld = beta
+            beta = self.betaNew(X,y,betaOld)
+            if self.null:
+                self.theta = beta
+                return self.theta
+        self.theta = beta
+        return self.theta
---
+++ prj2aworking/systems/simplepoly.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import lib.regressor as reg
+import lib.gradientdescent as gd
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class SimpleRegression:
+
+    def __init__(self):
+        self
+
+    def RunSimpleExample(self, dtpts):
+        from sklearn.linear_model import SGDRegressor
+        x = np.random.rand(dtpts,1) # random col matrix of positions
+        y = 4 + 3*x# + np.random.randn(dtpts,1) # polynomial with noise 
+
+        X = np.c_[np.ones((dtpts, 1)), x] # feature matrix w bias and inputs
+        
+        print("="*50, "\nBeta's:")
+
+        #Theta is the common nomenclature for optimization params. 
+        theta_ols = np.linalg.pinv( X.T@X ) @ ( X.T@y )
+        print("ols theta\n", theta_ols)
+
+        #skl
+        sklreg = SGDRegressor(max_iter=50, penalty=None, eta0=0.1)
+        sklreg.fit(x, y.ravel())
+        print("skl regr.\n", sklreg.intercept_, sklreg.coef_)
+
+        #SGD
+        sgrad = gd.SGD()
+        sgrad.SetLearningMode('static', .01)
+        sgrad.fit(X, y.ravel(), epochs=100, minibatches=100, lrn=.01)
+        print("own SGD\n", sgrad.theta)
+
+        print("="*50)
+        
