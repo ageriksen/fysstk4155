c1b37b6ec9be2bd816f6a9c9b4d63caf35e37623
ageriksen <annikage@protonmail.com>
2020-12-12 18:41:53 +0100

    splitAndScaledData

--- project3/src/sys/ffnn.py
+++ project3/src/sys/ffnn.py
@@ -23,17 +23,24 @@ import torch
 import torch.nn.functional as F
 
 torch.manual_seed(2020)
-#X = torch.tensor(
-#        torch.squeeze(torch.from_numpy(cancer.data), dim=1),
-#        dtype=torch.float32)
-#y = torch.from_numpy(cancer.target, dtype=torch.float32)
-#y = y.reshape(y.numel(), 1)
 
 X = torch.tensor(cancer.data, dtype=torch.float32)
 y = torch.tensor(cancer.target, dtype=torch.float32)
 
 X = X.reshape(y.numel(), len(cancer.feature_names))
 y = y.reshape(y.numel(), 1)
+
+from sklearn.preprocessing import StandardScaler 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+X_train_scaled = torch.tensor(X_train_scaled, dtype=torch.float32)
+X_test_scaled = torch.tensor(X_test_scaled, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.float32)
+y_test = torch.tensor(y_test, dtype=torch.float32)
 
 class Net(torch.nn.Module):
     def __init__(self, n_feature, n_hidden, n_output):
@@ -60,10 +67,10 @@ difference = np.zeros(epochs)
 for t in range(epochs):
 
     #print('starting prediction')
-    prediction = net(X) #predict based on x
+    prediction = net(X_train_scaled) #predict based on x
 
     #print('finding loss')
-    loss = loss_func(prediction, y)#( 1: nn output, 2: target )
+    loss = loss_func(prediction, y_train)#( 1: nn output, 2: target )
 
     #print('optimizing')
     optimizer.zero_grad()
@@ -72,7 +79,7 @@ for t in range(epochs):
     #print('step optimizer')
     optimizer.step()
     #print("this is where I'd find the score")
-    difference[t] = torch.mean(abs( torch.round(prediction) - y ) )
+    difference[t] = 1 - torch.mean(abs( torch.round(prediction) - y_train ) )
     #print("score found")
 
 
